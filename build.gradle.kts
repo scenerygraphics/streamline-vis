9b4d08bfe0dbb4f412066b9f7853264c4d58c6b5
Ulrik Guenther <hello@ulrik.is>
2022-11-14 22:40:03 +0100

    Gradle: Bump scenery and trx-jvm version to latest repo versions, add BioFormats for NiFTI reading

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,24 +3,35 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     application
+    `maven-publish`
 }
 
 group = "graphics.scenery"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal() {
+        content {
+            includeGroup("graphics.scenery")
+        }
+    }
     mavenCentral()
     maven("https://maven.scijava.org/content/groups/public")
     maven("https://jitpack.io")
 }
 
 dependencies {
-    api("graphics.scenery:scenery:222a631")
+    api("graphics.scenery:scenery:04798e0")
     api("org.slf4j:slf4j-simple:1.7.36")
     implementation("org.joml:joml:1.10.4")
     implementation("net.imglib2:imglib2:5.13.0")
-    implementation("com.github.scenerygraphics:trx-jvm:73070ac")
+    api("graphics.scenery:trx-jvm:47d1732")
 
+    implementation(platform("org.scijava:pom-scijava:32.0.0"))
+    implementation("io.scif:scifio")
+    runtimeOnly("io.scif:scifio-bf-compat")
+    runtimeOnly("ome:formats-bsd")
+    runtimeOnly("ome:formats-gpl")
 
     testImplementation(kotlin("test"))
 }
@@ -34,5 +45,5 @@ tasks.withType<KotlinCompile> {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("BasicStreamlineExample")
 }
